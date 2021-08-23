bd09e6a9bfba25817450ed1832adc4e689977d18
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-08-23 17:09:02 +0700

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,11 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
 #define ll long long
-#define SZ(x) (int)((x).size())
-const ll mod = 1e9 + 7;
-const ll base = 31;
-int n, hsh[100005];
+
+const ll mod = 1e9 + 7; //998244353
+const ll base = 31; //311
+
+ll n, hsh[100005];
 string s;
+
 /// thay đổi kí tự ở vị trí vis từ from -> to và
 /// tính hash mới của xâu độ dài n với has là hash cũ
 ll trans(ll has,char from,char to,int vis,int n){
@@ -17,11 +17,9 @@ ll trans(ll has,char from,char to,int vis,int n){
 ll cal(int l,int r){
     return (hsh[r] - hsh[l-1] * pw[r-l+1] + mod * mod) % mod;
 }
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
-//    freopen("c.inp","r",stdin);
-    cin >> s; n = SZ(s); s = ' ' + s; pw[0] = 1;
+
+void init(){
+    pw[0] = 1;
     for(int i = 1;i <= n;++i){
         pw[i] = (pw[i-1] * base) % mod;
         hsh[i] = (hsh[i-1] * base + s[i] - 'a' + 1) % mod;
