1fe39e6a8cc87acc1abe726adb09500e9917bc75
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-08-24 18:37:24 +0700

    Update hld.cpp

--- Tree/hld.cpp
+++ Tree/hld.cpp
@@ -1,1 +1,33 @@
+int n, sz[200005], d[200005], cha[200005];
+int head[200005], pos[200005], cnt;
+vector<int> a[200005];
 
+void DFS(int u){
+    sz[u] = 1;
+    for(int v : a[u])if(v != cha[u]){
+        d[v] = d[u] + 1;
+        cha[v] = u;
+        DFS(v);
+        sz[u] += sz[v];
+    }
+}
+
+void hld(int u,int top)
+{
+    pos[u] = ++cnt;
+    head[u] = top;
+    int best = - 1;
+    for(int v : a[u])if(v != cha[u])
+        if(best == -1 or sz[v] > sz[best])best = v;
+    if(best == -1)return;
+    hld(best,top);
+    for(int v : a[u])if(v != cha[u] && v != best)hld(v,v);
+}
+
+int lca(int u,int v){
+    while(head[u] != head[v]){
+        if(d[head[u]] < d[head[v]])swap(u,v);
+        u = cha[head[u]];
+    }
+    return d[u] < d[v] ? u : v;
+}
