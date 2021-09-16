90de8fdaf0e17925ae71cb3b94e11111eb07cf4a
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-09-16 21:15:32 +0700

    Update LAZY_SEGMENTREE.cpp

--- CTDL/LAZY_SEGMENTREE.cpp
+++ CTDL/LAZY_SEGMENTREE.cpp
@@ -1,13 +1,13 @@
 struct Lazy_Seg{
     int st[400005], lz[400005];
-    void build(int id,int l,int r,int a[]){
+    void build(int a[], int id = 1,int l = 1,int r = n){
         if(l == r){
             st[id] = a[l];
             return;
         }
         int mid = l + r >> 1;
-        build(id<<1, l, mid, a);
-        build(id<<1|1, mid+1, r, a);
+        build(a, id<<1, l, mid);
+        build(a, id<<1|1, mid+1, r);
         st[id] = st[id<<1] + st[id<<1|1];
     }
     void push(int id,int l,int r){
@@ -19,7 +19,7 @@ struct Lazy_Seg{
         lz[id<<1|1] += lz[id];
         lz[id] = 0;
     }
-    void update(int id,int l,int r,int u,int v,int val){
+    void update(int u,int v,int val, int id = 1,int l = 1,int r = n){
         if(l > v || r < u)return;
         if(u <= l && r <= v){
             st[id] += val;
@@ -28,15 +28,15 @@ struct Lazy_Seg{
         }
         push(id, l, r);
         int mid = l + r >> 1;
-        update(id<<1, l, mid, u, v, val);
-        update(id<<1|1, mid+1, r, u, v, val);
+        update(u, v, val, id<<1, l, mid);
+        update(u, v, val, id<<1|1, mid+1, r);
         st[id] = st[id<<1] + st[id<<1|1];
     }
-    int query(int id,int l,int r,int u,int v){
+    int get(int u,int v,int id = 1,int l = 1,int r = n){
         if(l > v or r < u)return 0;
         if(u <= l && r <= v)return st[id];
         push(id, l, r);
         int mid = l + r >> 1;
-        return query(id<<1, l, mid, u, v) + query(id<<1|1, mid+1, r, u, v);
+        return get(u, v, id<<1, l, mid) + query(u, v, id<<1|1, mid+1, r);
     }
 };
