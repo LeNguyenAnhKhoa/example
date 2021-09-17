03e95cdf00c2c9fe6d65dec10f75a3c5aaf231fc
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-09-17 16:48:11 +0700

    Update IT.cpp

--- CTDL/IT.cpp
+++ CTDL/IT.cpp
@@ -1,6 +1,6 @@
 struct IT{
     int st[400005];
-    void update(int id,int l,int r,int i,int val){
+    void update(int i,int val,int id = 1,int l = 1,int r = n){
         if(l == r){
             st[id] = val;
             return;
@@ -10,10 +10,10 @@ struct IT{
         else update(id<<1|1, mid+1, r, i, val);
         st[id] = st[id<<1] + st[id<<1|1];
     }
-    int get(int id,int l,int r,int u,int v){
+    int get(int u,int v, int id,int l,int r){
         if(l > v or r < u)return 0;
         if(u <= l && r <= v)return st[id];
         int mid = l + r >> 1;
-        return get(id<<1, l, mid, u, v) + get(id<<1|1, mid+1, r, u, v);
+        return max((id<<1, l, mid, u, v), get(id<<1|1, mid+1, r, u, v));
     }
 };
