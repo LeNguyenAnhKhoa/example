bc0bbda9daeb8f810a29b61c91336be32225e29f
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-09-24 20:28:25 +0700

    Update DP_Convexhull.cpp

--- Geometry/DP_Convexhull.cpp
+++ Geometry/DP_Convexhull.cpp
@@ -1,31 +1,30 @@
-#define int long long
-int mode;
-struct Line{
-    mutable int a, b, k;
-    bool operator<(const Line &p)const{return mode ? k < p.k : a < p.a;}
+#define ll long long
+struct Line {
+  mutable ll m, b, p;
+  bool operator<(const Line& o) const { return m < o.m; }
+  bool operator<(ll x) const { return p < x; }
 };
-struct ConvexHull{
-    multiset<Line> s;
-    const int INF = 2e18;
-    int div(int a,int b){
+struct LineContainer : multiset<Line, less<>> {
+  // (for doubles, use inf = 1/.0, div(a,b) = a/b)
+    const ll inf = LLONG_MAX;
+    ll div(ll a, ll b) { // floored division
         return a / b - ((a ^ b) < 0 && a % b);
     }
-    template<class T>bool isect(T x,T y) {
-        if(y == s.end()){x->k = INF; return 0;}
-        if(x->a == y->a) x->k = (x->b >= y->b ? INF : -INF);
-        else x->k = div(x->b - y->b, y->a - x->a);
-        return x->k >= y->k;
+    bool isect(iterator x, iterator y){
+        if (y == end()) { x->p = inf; return false; }
+        if (x->m == y->m) x->p = x->b > y->b ? inf : -inf;
+        else x->p = div(y->b - x->b, x->m - y->m);
+        return x->p >= y->p;
     }
-    void add(int a,int b) {
-        auto z = s.insert({a, b, 0}), x = z++, y = x;
-        while(isect(x, z)) z = s.erase(z);
-        if(x != s.begin() && isect(--x,y)) isect(x, s.erase(y));
-        while((y = x) != s.begin() && isect(--x,y)) isect(x, s.erase(y));
+    void add(ll m, ll b) {
+        auto z = insert({m, b, 0}), y = z++, x = y;
+        while (isect(y, z)) z = erase(z);
+        if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p) isect(x, erase(y));
     }
-    int get(int x) {
-        mode = 1;
-        auto pos = s.lower_bound({0, 0, x});
-        mode = 0;
-        return pos->a * x + pos->b;
+    ll get(ll x){
+        assert(!empty());
+        auto l = *lower_bound(x);
+        return l.m * x + l.b;
     }
 }cht;
