80d962e773d82bb2291f87ec62c4938d892e358f
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-10-02 21:00:08 +0700

    Update geometry.cpp

--- Geometry/geometry.cpp
+++ Geometry/geometry.cpp
@@ -1,18 +1,26 @@
+#define ll long long
 struct Point {
-    int x, y;
+    ll x, y;
     Point() { x = y = 0; }
-    Point(int x, int y) : x(x), y(y) {}
+    Point(ll x, ll y) : x(x), y(y) {}
 
     Point operator + (const Point& a) const { return Point(x+a.x, y+a.y); }
     Point operator - (const Point& a) const { return Point(x-a.x, y-a.y); }
-    Point operator * (int k) const { return Point(x*k, y*k); }
+    Point operator * (ll k) const { return Point(x*k, y*k); }
 
     int operator * (const Point& a) const { return x*a.x + y*a.y; } // dot product
     int operator % (const Point& a) const { return x*a.y - y*a.x; } // cross product
 
     Point conj() { return Point(x, -y); }
-    int norm() { return x*x + y*y; }
-    
+
+    ll norm() { return x*x + y*y; }
+
+    friend ll d(const Point &a,const Point &b){
+        ll x = (a.x - b.x); x *= x;
+        ll y = (a.y - b.y); y *= y;
+        return sqrt(x + y);
+    }
+
     bool operator <(const Point &a){
         return a.x == x ? y < a.y : x < a.x;
     }
@@ -23,6 +31,7 @@ struct Point {
         return x == a.x && y == a.y;
     }
 };
+
 
 ///left: ccw > 0
 ///right: ccw < 0
