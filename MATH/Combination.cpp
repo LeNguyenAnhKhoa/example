587020e288d7020c1a70008e43bd3ebb994bb835
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-10-11 21:06:00 +0700

    Update Combination.cpp

--- MATH/Combination.cpp
+++ MATH/Combination.cpp
@@ -9,9 +9,11 @@ int pw(int a,int n){
 }
 void init(){
     fac[0] = 1;
-    for(int i = 1;i <= n;++i)fac[i] = fac[i-1] * i % mod;
+    for(int i = 1;i <= n;++i)
+        fac[i] = fac[i-1] * i % mod;
     ifac[n] = pw(fac[n], mod-2);
-    for(int i = n;i >= 1;--i)ifac[i-1] = ifac[i] * i % mod;
+    for(int i = n;i >= 1;--i)
+        ifac[i-1] = ifac[i] * i % mod;
 }
 int C(int n,int k){
     return fac[n] * ifac[n-k] % mod * ifac[k] % mod;
