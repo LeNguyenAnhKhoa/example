88f7705838407f9e76ede18340cd5584f52d4a43
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-11-10 10:40:52 +0700

    Update RMQ.cpp

--- CTDL/RMQ.cpp
+++ CTDL/RMQ.cpp
@@ -1,12 +1,13 @@
-int n, jump[17][100005], a[100005];
+const int N = 1e5 + 5;
+int n, f[17][N], a[N];
 
 void init(){
-    for(int i = 1;i <= log2(n);++i)jump[0][i] = a[i];
-    for(int i = 1;i <= 16;++i)for(int j = 1;j + (1<<i) - 1 <= n;++j)
-        jump[i][j] = min(jump[i-1][j], jump[i-1][j+(1<<(i-1))]);
+    for(int i = 1;i <= n;++i)f[0][i] = a[i];
+    for(int i = 1;1<<i <= n;++i)for(int j = 1;j + (1<<i) - 1 <= n;++j)
+        f[i][j] = min(f[i-1][j], f[i-1][j+(1<<(i-1))]);
 }
 
 int get(int l,int r){
     int LOG = log2(r - l + 1);
-    return min(jump[LOG][l], jump[LOG][r-(1<<LOG)+1]);
+    return min(f[LOG][l], f[LOG][r-(1<<LOG)+1]);
 }
