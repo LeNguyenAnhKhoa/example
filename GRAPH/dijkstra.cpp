61d87f8cc7cfd199cdef86c38efeb3834a307a51
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-11-10 10:44:01 +0700

    Update dijkstra.cpp

--- GRAPH/dijkstra.cpp
+++ GRAPH/dijkstra.cpp
@@ -1,16 +1,18 @@
-int d[100005], n;
-vector<ii> a[100005];
-int dijkstra(int s,int t){
-    memset(d, 0x7f, sizeof d);
+#define fi first
+#define se second
+typedef pair<int,int> ii;
+const int N = 1e5 + 5;
+int n;
+vector<ii> a[N];
+vector<int> dijkstra(int s){
+    vector<int> d(n+5, 1e9);
     priority_queue<ii, vector<ii>, greater<ii>> q;
-    q.push({0, s}); d[s] = 0;
+    q.push({0, s}), d[s] = 0;
     while(!q.empty()){
         int u = q.top().se, du = q.top().fi; q.pop();
         if(du != d[u])continue;
-        for(auto v : a[u])if(d[v.se] > du + v.fi){
-            d[v.se] = du + v.fi;
-            q.push({d[v.se], v.se});
-        }
+        for(auto v : a[u])if(d[v.se] > du + v.fi)
+            d[v.se] = du + v.fi, q.push({d[v.se], v.se});
     }
-    return d[t];
+    return d
 }
