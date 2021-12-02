c2a2b93e15948bf3070c37b06a4ab03e2c76d4d4
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2021-12-02 15:33:10 +0700

    Update binary_jump.cpp

--- Tree/binary_jump.cpp
+++ Tree/binary_jump.cpp
@@ -1,19 +1,19 @@
 const int N = 1e5 + 5;
-int n, jump[17][N], d[N];
+int n, p[17][N], d[N];
 vector<int> a[N];
 void DFS(int u){
     for(int i = 1;1<<i <= n;++i)
-        jump[i][u] = jump[i-1][jump[i-1][u]];
-    for(auto v : a[u])if(v != jump[0][u])
+        p[i][u] = p[i-1][p[i-1][u]];
+    for(auto v : a[u])if(v != p[0][u])
         d[v] = d[u] + 1, DFS(v);
 }
 int lca(int u,int v){
     if(d[u] < d[v])swap(u, v);
     int diff = d[u] - d[v];
     for(int i = log2(n);i >= 0;--i)
-        if(diff>>i&1)u = jump[i][u];
+        if(diff>>i&1)u = p[i][u];
     if(u == v)return u;
-    for(int i = log2(n);i >= 0;--i)if(jump[i][u] != jump[i][v])
-        u = jump[i][u], v = jump[i][v];
-    return jump[0][u];
+    for(int i = log2(n);i >= 0;--i)if(p[i][u] != p[i][v])
+        u = p[i][u], v = p[i][v];
+    return p[0][u];
 }
