81fc057e2b7b5018de8850a942daa451ee0c9390
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-02-16 09:28:30 +0700

    Update binary_jump.cpp

--- Tree/binary_jump.cpp
+++ Tree/binary_jump.cpp
@@ -1,19 +1,19 @@
 const int N = 1e5 + 5;
 int n, p[17][N], d[N];
 vector<int> a[N];
-void DFS(int u){
-    for(int i = 1;1<<i <= n;++i)
+void dfs(int u){
+    FOR(i, 1, log2(n))
         p[i][u] = p[i-1][p[i-1][u]];
     for(auto v : a[u])if(v != p[0][u])
-        d[v] = d[u] + 1, DFS(v);
+        d[v] = d[u] + 1, p[0][v] = u, dfs(v);
 }
-int lca(int u,int v){
+int lca(int u, int v){
     if(d[u] < d[v])swap(u, v);
     int diff = d[u] - d[v];
-    for(int i = log2(n);i >= 0;--i)
+    FOD(i, log2(n), 0)
         if(diff>>i&1)u = p[i][u];
     if(u == v)return u;
-    for(int i = log2(n);i >= 0;--i)if(p[i][u] != p[i][v])
+    FOD(i, log2(n), 0)if(p[i][u] != p[i][v])
         u = p[i][u], v = p[i][v];
     return p[0][u];
 }
