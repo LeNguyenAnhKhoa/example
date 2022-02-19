387eefc008e592f3d8f7292c3b70c91546a986e8
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-02-19 08:05:48 +0700

    Update IT.cpp

--- CTDL/IT.cpp
+++ CTDL/IT.cpp
@@ -2,6 +2,16 @@ const int N = 1e5 + 5;
 int n;
 struct IT{
     int st[4*N];
+    void build(int id = 1, int l = 1, int r = n){
+        if(l == r){
+            st[id] = a[l];
+            return;
+        }
+        int mid = l + r >> 1;
+        build(id<<1, l, mid);
+        build(id<<1|1, mid+1, r);
+        st[id] = min(st[id<<1], st[id<<1|1]);
+    }
     void add(int i, int val, int id = 1, int l = 1, int r = n){
         if(l == r){
             st[id] = val;
@@ -10,12 +20,12 @@ struct IT{
         int mid = l + r >> 1;
         if(i <= mid)add(i, val, id<<1, l, mid);
         else add(i, val, id<<1|1, mid+1, r);
-        st[id] = max(st[id<<1], st[id<<1|1]);
+        st[id] = min(st[id<<1], st[id<<1|1]);
     }
     int get(int u, int v, int id = 1, int l = 1, int r = n){
-        if(l > v or r < u)return 0;
+        if(l > v or r < u)return 1e9;
         if(u <= l && r <= v)return st[id];
         int mid = l + r >> 1;
-        return max(get(u, v, id<<1, l, mid), get(u, v, id<<1|1, mid+1, r));
+        return min(get(u, v, id<<1, l, mid), get(u, v, id<<1|1, mid+1, r));
     }
 };
