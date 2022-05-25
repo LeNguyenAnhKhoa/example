6a560b994200033bcec9d01e05fecccdc70127c1
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-05-25 14:53:39 +0700

    Update DSU.cpp

--- GRAPH/DSU.cpp
+++ GRAPH/DSU.cpp
@@ -1,17 +1,16 @@
 const int N = 1e5 + 5;
-int p[N], sz[N], n;
-int root(int u){
-    return u == p[u] ? u : p[u] = root(p[u]);
-}
-bool join(int u,int v){
-    u = root(u), v = root(v);
-    if(u == v)return 0;
-    if(sz[u] < sz[v])swap(u, v);
-    sz[u] += sz[v];
-    p[v] = u;
-    return 1;
-}
-void init(){
-    for(int i = 1;i <= n;++i)
-        cha[i] = i, sz[i] = 1;
-}
+struct dsu{
+    int p[N];
+    void init(){
+        FOR(i, 1, n)
+            p[i] = -1;
+    }
+    int root(int u){
+        return p[u] < 0 ? u : p[u] = root(p[u]);
+    }
+    bool join(){
+        if((u = root(u)) == (v = root(v)))return 0;
+        if(p[u] > p[v])swap(u, v);
+        p[u] += p[v], p[v] = u;
+    }
+};
