0c8811da1e0d868b422001e168f8da8ac8ee1a4b
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-07-08 13:10:04 +0700

    Update BIT.cpp

--- CTDL/BIT.cpp
+++ CTDL/BIT.cpp
@@ -4,12 +4,12 @@ struct BIT{
     int bit[N];
     void add(int i, int x){
         if(i == 0)return;
-        for(;i <= n;i += i&-i)
+        for(; i <= n; i += i&-i)
             bit[i] += x;
     }
     int get(int i){
         int res = 0;
-        for(;i > 0;i ^= i&-i)
+        for(; i > 0; i ^= i&-i)
             res += bit[i];
         return res;
     }
