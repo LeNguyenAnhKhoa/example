0dc0f9e962324e9675c06fb1796df7bfce737bf3
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-07-31 10:04:09 +0700

    Update Combination.cpp

--- MATH/Combination.cpp
+++ MATH/Combination.cpp
@@ -18,7 +18,7 @@ void init(){
     C[0][0] = 1;
     FOR(i, 1, n){
         C[i][0] = C[i][i] = 1;
-        FOR(j, 1, n-1)
+        FOR(j, 1, i-1)
             C[i][j] = (C[i-1][j-1] + C[i-1][j]) % mod;
     }
 }
