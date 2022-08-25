6fd2e8684660f65028be174211a6b906aa4564c2
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-08-25 15:13:07 +0700

    Update Combination.cpp

--- MATH/Combination.cpp
+++ MATH/Combination.cpp
@@ -2,17 +2,21 @@
 const int mod = 1e9 + 7;
 const int N = 1e5 + 5;
 int fac[N], n, ifac[N], C[N][N];
-int pw(int a, int n){
-    if(n == 1)return a;
-    int tmp = pw(a, n/2);
-    if(n&1)return tmp * tmp % mod * a % mod;
-    return tmp * tmp % mod;
+int cal(int x, int y){
+    int res = 1;
+    while(y){
+        if(y & 1)
+            res = res * x % mod;
+        x = x * x % mod;
+        y >>= 1;
+    }
+    return res;
 }
 void init(){
     fac[0] = 1;
     for(int i = 1; i <= n; ++i)
         fac[i] = fac[i-1] * i % mod;
-    ifac[n] = pw(fac[n], mod-2);
+    ifac[n] = cal(fac[n], mod-2);
     for(int i = n; i >= 1; --i)
         ifac[i-1] = ifac[i] * i % mod;
     C[0][0] = 1;
@@ -22,7 +26,7 @@ void init(){
             C[i][j] = (C[i-1][j-1] + C[i-1][j]) % mod;
     }
 }
-int C(int n,int k){
+int C(int n, int k){
     if(k > n)return 0;
     return fac[n] * ifac[n-k] % mod * ifac[k] % mod;
 }
