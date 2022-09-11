f4a8819fede22afef228e5453ac6569cc4edbc72
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-09-11 20:46:28 +0700

    Update binary_jump2.cpp

--- Tree/binary_jump2.cpp
+++ Tree/binary_jump2.cpp
@@ -6,7 +6,7 @@ void DFS(int u){
     FOR(i, 1, l)p[i][u] = p[i-1][p[i-1][u]];
     for(auto v : a[u])if(v != p[0][u])
         p[0][v] = u, DFS(v);
-    out[u] = ++t;
+    out[u] = t;
 }
 void init(){
     l = log2(n);
@@ -18,8 +18,7 @@ bool ck(int u, int v){
     return in[u] <= in[v] && out[v] <= out[u];
 }
 int lca(int u, int v){
-    if(ck(u, v))return u;
-    if(ck(v, u))return v;
+    if(ck(u, v))return u; if(ck(v, u))return v;
     FOD(i, l, 0)if(!ck(p[i][u], v))u = p[i][u];
     return p[0][u];
 }
