7fece26ed9c72b841e8a77d9459aef83c91145c5
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-09-18 18:27:36 +0700

    Update flow.cpp

--- GRAPH/flow.cpp
+++ GRAPH/flow.cpp
@@ -1,62 +1,56 @@
-struct edge{
-    int v, cap, flow;
-};
-vector<edge> e;
+struct edge{int u, v, cap, flow;};
+int n, s, t, m, cur[N], d[N], c[N][N];
 vector<int> a[N];
-int n, m, s, t, ne, d[N], cnt, vis[N], cur[N], sz[N];
-void add(int u, int v, int cap){
-    e.pb({v, cap, 0});
-    e.pb({u, 0, 0});
-    a[u].pb(ne);
-    a[v].pb(ne+1);
-    ne += 2;
+vector<edge> e;
+void add(int u,int v,int cap){
+    edge e1 = {u, v, cap, 0};
+    edge e2 = {v, u, 0, 0};
+    a[u].pb(SZ(e)); e.pb(e1);
+    a[v].pb(SZ(e)); e.pb(e2);
 }
 bool bfs(){
     queue<int> q;
-    q.push(s);
-    memset(d, 0, sizeof d);
-    d[s] = 1;
-    while(!q.empty()){
+    memset(d, -1, sizeof d);
+    q.push(s); d[s] = 0;
+    while(!q.empty() && d[t] < 0){
         int u = q.front(); q.pop();
-        if(u == t)return 1;
-        for(auto id : a[u]){
-            if(d[e[id].v])continue;
-            if(e[id].cap - e[id].flow > 0){
-                d[e[id].v] = d[u] + 1;
-                q.push(e[id].v);
-            }
+        for(auto i : a[u]){
+            int v = e[i].v;
+            if(d[v] < 0 && e[i].flow < e[i].cap)
+                q.push(v), d[v] = d[u] + 1;
+        }
+    }
+    return d[t] >= 0;
+}
+int dfs(int u,int val){
+    if(!val)return 0;
+    if(u == t)return val;
+    for(;cur[u] < SZ(a[u]);++cur[u]){
+        int i = a[u][cur[u]], v = e[i].v;
+        if(d[v] != d[u] + 1)continue;
+        int pushed = dfs(v, min(val, e[i].cap - e[i].flow));
+        if(pushed){
+            e[i].flow += pushed;
+            e[i^1].flow -= pushed;
+            return pushed;
         }
     }
     return 0;
 }
-int dfs(int u, int pushed){
-    if(u == t)return pushed;
-    vis[u] = cnt;
+int maxflow(){
     int res = 0;
-    for(; cur[u] < sz[u]; ++cur[u]){
-        int id = a[u][cur[u]];
-        if(d[e[id].v] != d[u] + 1)continue;
-        if(vis[e[id].v] == cnt)continue;
-        if(e[id].flow < e[id].cap){
-            int f = dfs(e[id].v, min(pushed, e[id].cap - e[id].flow));
-            res += f;
-            pushed -= f;
-            e[id].flow += f;
-            e[id^1].flow -= f;
+    while(bfs()){
+        memset(cur, 0, sizeof cur);
+        while(1){
+            int val = dfs(s, 1e9);
+            if(!val)break;
+            res += val;
         }
     }
     return res;
 }
-int maxflow(){
-    FOR(i, 1, n)sz[i] = SZ(a[i]);
-    int res = 0;
-    while(1){
-        if(!bfs())break;
-        cnt++;
-        memset(cur, 0, sizeof cur);
-        int f = dfs(s, 1e9);
-        if(!f)break;
-        res += f;
-    }
-    return res;
+void init(){
+    e.clear();
+    FOR(i, 0, 2*n+1)a[i].clear();
+    s = 0, t = 2*n+1;
 }
