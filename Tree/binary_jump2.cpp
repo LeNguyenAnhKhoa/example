7c719f3008549f19882da00bd4c1f5768e7a019e
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-11-04 12:53:15 +0700

    Update binary_jump2.cpp

--- Tree/binary_jump2.cpp
+++ Tree/binary_jump2.cpp
@@ -11,14 +11,12 @@ void DFS(int u){
 void init(){
     l = log2(n);
     DFS(1);
-    in[0] = -1e9;
-    out[0] = 1e9;
 }
 bool ck(int u, int v){
     return in[u] <= in[v] && out[v] <= out[u];
 }
 int lca(int u, int v){
     if(ck(u, v))return u; if(ck(v, u))return v;
-    FOD(i, l, 0)if(!ck(p[i][u], v))u = p[i][u];
+    FOD(i, l, 0)if(v, p[i][u]))u = p[i][u];
     return p[0][u];
 }
