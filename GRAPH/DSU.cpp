7248cf14d0deeba5a13620801a38cdc074832693
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-11-07 14:08:46 +0700

    Update DSU.cpp

--- GRAPH/DSU.cpp
+++ GRAPH/DSU.cpp
@@ -1,12 +1,9 @@
 const int N = 1e5 + 5;
 int p[N];
-FOR(i, 1, n)
-    p[i] = -1;
-}
 int root(int u){
     return p[u] < 0 ? u : p[u] = root(p[u]);
 }
-bool join(){
+bool join(int u, int v){
     if((u = root(u)) == (v = root(v)))return 0;
     if(p[u] > p[v])swap(u, v);
     p[u] += p[v], p[v] = u;
