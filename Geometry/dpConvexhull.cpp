c94b93eb1425123818bb03d5f3bac5bd2e8023c1
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2022-11-25 09:44:03 +0700

    Update dpConvexhull.cpp

--- Geometry/dpConvexhull.cpp
+++ Geometry/dpConvexhull.cpp
@@ -1,18 +1,19 @@
 /// return max(y) <=> "<="
 /// return min(y) <=> ">="
 bool cmp(ii a, ii b, ii c){
-    return (double)(b.se - a.se) / (a.fi - b.fi) >= (double)(c.se - a.se) / (double)(a.fi - c.fi);
+    return (double)(b.se - a.se) / (a.fi - b.fi) <= (double)(c.se - a.se) / (double)(a.fi - c.fi);
 }
-void add(int a, int b){
+void add(int a, int b, int i){
     while(SZ(line) >= 2 && cmp(line[SZ(line)-2], line.back(), ii(a, b)))line.pop_back();
     line.pb(a, b);
+    id.pb(i);
 }
 int cal(ii x, int a){
     return x.fi * a + x.se;
 }
-int get(int x){
+ii get(int x){
     int l = 0, r = SZ(line) - 1, res = 0;
-    while(l <= r){
+    while(l < r){
         int mid = l + r >> 1;
         if(mid == SZ(line) - 1){
             res = mid;
@@ -21,8 +22,8 @@ int get(int x){
         /// return max(y) <=> cal(line[mid], x) > cal(line[mid+1], x)
         /// return min(y) <=> cal(line[mid], x) < cal(line[mid+1], x)
         if(cal(line[mid], x) > cal(line[mid+1], x))
-            res = mid, r = mid - 1;
+            res = mid, r = mid;
         else l = mid + 1;
     }
-    return cal(line[res], x);
+    return ii(cal(line[res], x), id[res]);
 }
