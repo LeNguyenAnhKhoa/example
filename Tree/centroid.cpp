38f786dad6f20c983a676f16a3c08a70ba5eded6
LeNguyenAnhKhoa <81629306+LeNguyenAnhKhoa@users.noreply.github.com>
2023-01-12 15:46:07 +0700

    Update centroid.cpp

--- Tree/centroid.cpp
+++ Tree/centroid.cpp
@@ -1,20 +1,14 @@
-const int N = 2e5 + 5;
-int n, sz[N], vis[N], pa[N];
-vector<int> a[N];
-
 int dfs(int u, int p = 0){
     sz[u] = 1;
     for(auto v : a[u])if(!vis[v] && v != p)
         sz[u] += dfs(v, u);
     return sz[u];
 }
-
 int centroid(int m, int u, int p = 0){
     for(auto v : a[u])if(!vis[v] && v != p && sz[v] > m)
         return centroid(m, v, u);
     return u;
 }
-
 void build(int u = 1, int p = 0){
     int c = centroid(dfs(u)>>1, u);
     vis[c] = 1, pa[c] = p;
